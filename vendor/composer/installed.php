2d76498e27945d636c19518334031ee7c6d7b504
Rodolfo Romaioli Ribeiro de Jesus <rrrjesus@users.noreply.github.com>
2024-08-08 11:09:57 -0300

    Updates FrontEnd
    Updates FrontEnd

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'smsub/smsubcontrol',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '1b0fb6bc0c3f585d3dfe7b40df62506d09f58f25',
+        'reference' => '49382971200f85746c6b34fd8fc831ec3eba3139',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -112,11 +112,26 @@
         'smsub/smsubcontrol' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '1b0fb6bc0c3f585d3dfe7b40df62506d09f58f25',
+            'reference' => '49382971200f85746c6b34fd8fc831ec3eba3139',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v5.3.3',
+            'version' => '5.3.3.0',
+            'reference' => '6e1f75f420f68e1d52733b8e407fc7c3766c9dba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v5.3.3',
+            ),
         ),
     ),
 );
